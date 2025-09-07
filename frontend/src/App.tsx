33ac74037bbe0c3976a654e6f3917270cdb686fa
patarr19NEO <yurapatarr19@yandex.ru>
2025-09-07 21:14:26 +0300

    added account files and connected em in apptsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from "./components/Header/Header.tsx"
 import {EnterForm} from "./components/EnterForm/EnterForm.tsx";
+import {Account} from "./components/Account/Account.tsx"
 
 function App() {
 
---
+++ frontend/src/components/Account/Account.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./Account.css"
+
+export default function Account() {
+    return (
+        <div className="Account">
+            <h1>While in progress</h1>
+        </div>
+    )
+}
