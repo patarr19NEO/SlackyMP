0845d41cc96b84e2c20d74d7606f94ba68a77c5d
patarr19NEO <yurapatarr19@yandex.ru>
2025-09-12 23:01:28 +0300

    added buttons"

--- frontend/src/components/Account/Account.tsx
+++ frontend/src/components/Account/Account.tsx
@@ -5,7 +5,7 @@ export default function Account() {
     return (
         <div className="Account">
             <h1>Hi, {localStorage.getItem("user")}</h1>
-            <div>
+            <div className="main-buttons">
                 <a>Заказы к выдачи</a>
                 <a>Выдать заказ</a>
             </div>
