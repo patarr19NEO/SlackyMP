b1f0020910a6e3aad1a684292448995527de86bb
patarr19NEO <yurapatarr19@yandex.ru>
2025-09-13 16:17:24 +0300

    added orderstobegiven logic

--- frontend/src/components/OrdersToBeGiven/OrdersToBeGiven.tsx
+++ frontend/src/components/OrdersToBeGiven/OrdersToBeGiven.tsx
@@ -1,9 +1,35 @@
 import "./OrdersToBeGiven.css"
-
+import { useState } from "react"
 export default function OrdersToBeGiven() {
+    const [orderId, setOrderId] = useState<any>()
+
+    const handleToBeGivenOrder = async () => {
+        if (!orderId) return
+
+        try {
+            const response = await fetch("http://127.0.0.1:5000/api/orders/${orderId}/issue", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if (response.ok) {
+                alert("Заказ выдан")
+                setOrderId("")
+            } else {
+                alert("Не найден id товара")
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return(
         <div className="OrdersToBeGiven">
             Заказы к выдаче
+            <input type="text" value={orderId} onChange={(e) => e.target.value} />
+            <button onClick={handleToBeGivenOrder}>Выдать</button>
         </div>
     )
 }
