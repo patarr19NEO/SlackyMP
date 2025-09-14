69e9a78735b03fd05aad04efdafb6e1f9a9eea41
patarr19NEO <yurapatarr19@yandex.ru>
2025-09-14 14:31:27 +0300

    some changes

--- frontend/src/components/OrdersToBeGiven/OrdersToBeGiven.tsx
+++ frontend/src/components/OrdersToBeGiven/OrdersToBeGiven.tsx
@@ -1,21 +1,25 @@
 import "./OrdersToBeGiven.css"
 import { useState } from "react"
 export default function OrdersToBeGiven() {
-    const [orderId, setOrderId] = useState<any>()
+    const [orderId, setOrderId] = useState<string>("")
 
     const handleToBeGivenOrder = async () => {
-        if (!orderId) return
+        if (!orderId) {
+            alert("Введите номер заказа!")
+        }
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/api/orders/${orderId}/issue", {
+            const response = await fetch(`http://127.0.0.1:5000/api/orders/${orderId}/issue`, {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json',
                 },
             })
 
+            const data = await response.json
+
             if (response.ok) {
-                alert("Заказ выдан")
+                alert(`Заказ выдан`)
                 setOrderId("")
             } else {
                 alert("Не найден id товара")
@@ -27,8 +31,7 @@ export default function OrdersToBeGiven() {
 
     return(
         <div className="OrdersToBeGiven">
-            Заказы к выдаче
-            <input type="text" value={orderId} onChange={(e) => e.target.value} />
+            <input type="text" value={orderId} onChange={(e) => setOrderId(e.target.value)} />
             <button onClick={handleToBeGivenOrder}>Выдать</button>
         </div>
     )
